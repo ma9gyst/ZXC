3f7c70f60ca57ffcbbda8444d3fce4ad0dd7f8c4
student <student@09-04>
2020-10-08 16:09:00 +0300

    1111

---
+++ 1111/Models/Suffix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1111.Models
+{
+    public class Suffix
+    {
+        public static string sb 
+        {
+            get { return "_sb.png"; }
+        }
+        public static string lg
+        {
+            get { return "_lg.png"; }
+        }
+        public static string full
+        {
+            get { return "_full.png"; }
+        }
+        public static string vert
+        {
+            get { return "_vert.jpg"; }
+        }
+    }
+}
